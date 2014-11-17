2bc144b164e5ac6be8955894d5bb3eb5eabec5d4
Carlos Araya <carlos.araya@gmail.com>
2014-11-17 07:23:45 -0800

    switched to minimized libraries

--- js/projectlist-fb.js
+++ js/projectlist-fb.js
@@ -2,8 +2,15 @@ projectApp = angular.module("projectList", ["firebase"]);
 
 projectApp.controller("projectListCtrl", ['$scope', '$firebase',
 function ($scope, $firebase) {
+  //Firebase-related
   var ref = new Firebase("https://project-list.firebaseio.com/");
   $scope.projects = $firebase(ref).$asArray();
+  
+  ref.authWithOAuthPopup("github", function(error, authData) {
+  
+  });
+
+  // project attributes
   $scope.appTitle = "Portfolio Project List";
   $scope.appAuthor = "Carlos Araya";
   }
