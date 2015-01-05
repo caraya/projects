f95daa3064c1ea3341f52c51a5a97a1cf0275695
Carlos Araya <carlos.araya@gmail.com>
2015-01-05 15:59:25 -0800

    got routing to work. Now to finish the wiring for edit, new and delete

--- app/js/core.js
+++ app/js/core.js
@@ -1,12 +1,29 @@
 /* jshint undef: true, unused: true, strict:false */
+/* jslint node: true */
 /* global angular:true */
-/*jslint node: true */
+
 'use strict';
 // Defining the core of the application
 // Handles mongolab connection and directives used in more than
 // one module. We might want to move everything  to their
 // own files but let's make sure they work first
-var projectApp = angular.module('projectCore', ['mongolabResourceHttp', 'hc.marked']);
+var projectApp = angular.module('projectCore', ['mongolabResourceHttp', 'hc.marked', 'ngRoute'], function ($routeProvider) {
+
+  $routeProvider
+    .when('/grid', {templateUrl: 'partials/grid.html', controller: 'ProjectListController', resolve: {
+      projects: function (Project) {return Project.all(); }
+    }})
+
+    .when('/edit/:id', {templateUrl: 'partials/form.html', controller: 'ProjectFormController', resolve: {
+      project: function (Project, $route) {return Project.getById($route.current.params.id); }
+    }})
+
+    .when('/new', {templateUrl: 'partials/form.html', controller: 'ProjectFormController', resolve: {
+      project: function (Project) {return new Project(); }
+    }})
+
+    .otherwise({redirectTo: '/grid'});
+});
 
 projectApp.constant('MONGOLAB_CONFIG', {
   API_KEY: '3n08EN4F5HQ2ndsob7ZA2o2xxRX1FuTq',
@@ -26,12 +43,37 @@ projectApp.config(['markedProvider',
         return hljs.highlightAuto(code).value;
       }
     });
-}]);
-
+  }]);
 
 projectApp.controller('ProjectListController', function ($scope, Project) {
   Project.all().then(function (projects) {
     $scope.projects = projects;
   });
   $scope.appTitle = 'Project List';
+});
+
+projectApp.controller('ProjectFormController', function($scope, $location, project) {
+
+    var projectCopy = angular.copy(project);
+    var changeSuccess = function() {
+      $location.path('/grid');
+    };
+
+    var changeError = function() {
+      throw new Error('Shit went wrong...');
+    };
+
+    $scope.project = project;
+
+    $scope.save = function(){
+      $scope.project.$saveOrUpdate(changeSuccess, changeSuccess, changeError, changeError);
+    };
+
+    $scope.remove = function() {
+      $scope.project.$remove(changeSuccess, changeError);
+    };
+
+    $scope.hasChanges = function(){
+      return !angular.equals($scope.project, projectCopy);
+    };
 });
